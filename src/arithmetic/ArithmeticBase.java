b46134efa00b2b538396f87e489597e7316bd00b
kau11117 <kau11117@sheridancollege.ca>
2022-02-14 10:48:55 -0500

    version1

--- src/arithmetic/ArithmeticBase.java
+++ src/arithmetic/ArithmeticBase.java
@@ -1,4 +1,5 @@
 /*
+name: Gueleen Kaur
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
